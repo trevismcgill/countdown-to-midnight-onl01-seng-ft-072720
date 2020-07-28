0a03aef10e5077d74276fa7b5a8b227c11a95c77
Trevis McGill <trevismcgill25@gmail.com>
2020-07-28 15:37:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
-
+seconds = 5
 def countdown
+  while seconds >0
+    puts "#{seconds} SECONDS!"
+    seconds -= 1
+  end
+  if seconds == 0
+    puts "HAPPY NEW YEAR!"
+  end
 end
+
+def countdown_with_sleep
